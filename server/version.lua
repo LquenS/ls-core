e3d4f64b7e117435c23ce626011e4ad5ee29bf94
LquenS <35933753+LquenS@users.noreply.github.com>
2022-12-12 14:39:56 +0300

    Version fixes

--- server/version.lua
+++ server/version.lua
@@ -1,16 +1,20 @@
 LS_CORE.Functions.GetVersionScript = function(CURRENT_VERSION, SCRIPT_NAME)
-    PerformHttpRequest("https://raw.githubusercontent.com/LquenS/ls-core/main/versions.json", function (_, data, __)
-        local SCRIPT_LIST = json.decode(RemoveLastLine(data))
-        for _, value in pairs ( SCRIPT_LIST ) do 
-            if value.name == SCRIPT_NAME then
-                print("[ls-core] " .. SCRIPT_NAME .. " checking started.")
-                Citizen.Wait(500)
-                if value.version == CURRENT_VERSION then
-                    print("[ls-core] " ..SCRIPT_NAME.. " version is latest\n[ls-core] Version name " .. value.version_name..".\n".."[ls-core] " .. value.version_desc..".")
-                else
-                    print("[ls-core] " ..SCRIPT_NAME.. " is outdated, needs to be updated!\n[ls-core] Latest version is " .. value.version .. ".")
+    PerformHttpRequest("https://raw.githubusercontent.com/LquenS/ls-core/main/versions.json?token=GHSAT0AAAAAAB4JAJD4DRBNO5RITOPDFS5GY4XCJSQ", function (_, data, __)
+        if data ~= nil then
+            local SCRIPT_LIST = json.decode(RemoveLastLine(data))
+            for _, value in pairs ( SCRIPT_LIST ) do 
+                if value.name == SCRIPT_NAME then
+                    print("[ls-core] " .. SCRIPT_NAME .. " checking started.")
+                    Citizen.Wait(500)
+                    if value.version == CURRENT_VERSION then
+                        print("[ls-core] " ..SCRIPT_NAME.. " version is latest\n[ls-core] Version name " .. value.version_name..".\n".."[ls-core] " .. value.version_desc..".")
+                    else
+                        print("[ls-core] " ..SCRIPT_NAME.. " is outdated, needs to be updated!\n[ls-core] Latest version is " .. value.version .. ".")
+                    end
                 end
             end
+        else
+            print("[ls-core] Versions cannot accessiable. Wait do not distrub dev, it\'s will pass soon!")
         end
     end)
 end
