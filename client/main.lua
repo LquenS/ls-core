411e62e1ddcb66cb0f253cd5cafc4421fc9d3dcc
LquenS <35933753+LquenS@users.noreply.github.com>
2022-12-13 01:40:28 +0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -2,9 +2,15 @@ LS_CORE = { }
 LS_CORE.Config = LS_CORE_CONFIG
 LS_CORE.PLAYER_DATA = {}
 
+LS_CORE.Functions = {}
+
 RegisterNetEvent('LS_CORE:PLAYER:SETPLAYERDATA', function(val)
     LS_CORE.PLAYER_DATA = val
 end)
+
+LS_CORE.Functions.GetPlayerData = function()
+    return LS_CORE.PLAYER_DATA
+end
 
 exports('GetCoreObject', function()
     return LS_CORE
