76d4e570ef4099623c008be1e1b73b11fe1becf8
LquenS <atakan.ts6161@gmail.com>
2023-06-20 10:09:22 +0300

    Version checker changed, little bit annoying

--- server/version.lua
+++ server/version.lua
@@ -4,15 +4,12 @@ LS_CORE.Functions.GetVersionScript = function(CURRENT_VERSION, SCRIPT_NAME)
             local SCRIPT_LIST = json.decode(data)
             for _, value in pairs ( SCRIPT_LIST ) do 
                 if value.name == SCRIPT_NAME then
-                    print("[================================================================================]\n")
-                    print("^1["..SCRIPT_NAME.."] ^7 checking started.")
                     if value.version == CURRENT_VERSION then
-                        print("^2[" ..SCRIPT_NAME.. "] VERSION IS LATEST\n[" ..SCRIPT_NAME.. "] VERSION TITLE ^3" .. value.version_name.."^2.\n".."[" ..SCRIPT_NAME.. "] ^3" .. value.version_desc.."^2.^7")
+                        print("^2[" ..SCRIPT_NAME.. "] VERSION IS LATEST\n[" ..SCRIPT_NAME.. "] VERSION TITLE ^3" .. value.version_name.."^2\n".."[" ..SCRIPT_NAME.. "] ^3" .. value.version_desc.."^2.^7")
                     else
                         print("^8[" ..SCRIPT_NAME.. "] ^1IS OUTDATED, NEEDS TO BE UPDATED!^8\n[" ..SCRIPT_NAME.. "] ^1LATEST VERSION IS^8 ^3" .. value.version .. "^8.^7")
-                        LS_CORE.Functions.CreateUpdateLoop("[================================================================================]\n".."^8[" ..SCRIPT_NAME.. "] ^1IS OUTDATED, NEEDS TO BE UPDATED!^8\n[" ..SCRIPT_NAME.. "] ^1LATEST VERSION IS^8 ^3" .. value.version .. "^8.^7".."\n[================================================================================]")
+                        LS_CORE.Functions.CreateUpdateLoop("^8[" ..SCRIPT_NAME.. "] ^1IS OUTDATED, NEEDS TO BE UPDATED!^8\n[" ..SCRIPT_NAME.. "] ^1LATEST VERSION IS^8 ^3" .. value.version .. "^8.^7")
                     end
-                    print("\n[================================================================================]")
                 end
             end
         else
@@ -24,7 +21,7 @@ end
 LS_CORE.Functions.CreateUpdateLoop = function(PRINT)
     Citizen.CreateThread(function()
         while true do
-            Citizen.Wait(2500)
+            Citizen.Wait(60000)
             print(PRINT)
         end
     end)
